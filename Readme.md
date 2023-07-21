e69b7f2d31124a295ff78d4c93ea839d0605fafa
afreen0102 <afreenw08@gmail.com>
2023-07-21 18:57:36 +0530

    dashboard setup is done

---
+++ Readme.md
@@ -0,0 +1,6 @@
+ npm install styled-components
+
+ npm i react-icons
+
+ npm i react-router-dom
+ 
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,27 @@
+import { css, styled } from "styled-components";
+
+// const test = css`text-align: center;
+// ${10 > 5 && 'background-color: yellow'}
+// `;
+
+
+const Heading = styled.h1`
+
+${(props) => 
+ props.type === 'h1' &&
+  css`
+    font-size: 3rem;
+    font-weight: 600;
+  `
+}
+
+${props => 
+props.type === 'h2' &&
+  css`
+    font-size: 2rem;
+    font-weight: 600;
+  `}
+
+`;
+
+export default Heading;
