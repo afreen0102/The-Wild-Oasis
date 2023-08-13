1f588c897fc739583623b97b7e75c07324392186
afreen0102 <afreenw08@gmail.com>
2023-08-13 11:44:18 +0530

    the section is completed

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -1,7 +1,9 @@
 import { createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiEllipsisVertical } from "react-icons/hi2";
+// import { HiEllipsisVertical } from "react-icons/hi";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const Menu = styled.div`
   display: flex;
@@ -97,24 +99,35 @@ function Toggle({id}){
   }
 
 
-  return <styledToggle onClick={handleClick}>
+  return <StyledToggle onClick={handleClick}>
     <HiEllipsisVertical/>
-  </styledToggle>
+  </StyledToggle>
 }
 
 function List({id, children }) {
-  const { openId, position } = useContext(MenusContext)
+  const { openId, position, close } = useContext(MenusContext);
+  const ref = useOutsideClick(close);
 
   if (openId !== id) return null;
 
   return createPortal(
-    <StyledList position={position}>{children}</StyledList>, document.body);
+    <StyledList position={position} ref={ref}>{children}</StyledList>, document.body);
   
 }
 
-function Button({children}) {
+function Button({children, icon, onClick}) {
+  const { close} = useContext(MenusContext)
+
+  function handleClick() {
+    onClick?.()
+    close()
+  }
+
+
   return <li>
-    <StyledButton>{children}</StyledButton>
+    <StyledButton onClick={handleClick}>
+      {icon}<span>{children}</span>
+      </StyledButton>
   </li>
 }
 
--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -86,8 +86,8 @@ function Row({children}) {
   </StyledRow>
 
 }
-function Body({data, render }) {
-  if(data.length) return <Empty>No data to show at the moment</Empty>
+function Body({ data, render }) {
+  if(!data.length) return <Empty>No data to show at the moment</Empty>
 
   return <StyledBody>
     {data.map(render)}
