aea761d0f3c2972d694696d4b22fb5ef361bbf26
afreen0102 <afreenw08@gmail.com>
2023-08-24 17:39:46 +0530

    starting with the charts

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import {useCabins} from "../cabins/useCabins";
+
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -9,9 +15,21 @@ const StyledDashboardLayout = styled.div`
 
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+  
+  const {cabins, isLoading: isLoading3 } = useCabins();
+
+  if(isLoading1 || isLoading2 || isLoading3) return <Spinner />
+
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats bookings={bookings} 
+      confirmedStays={confirmedStays} 
+      numDays={numDays}
+      cabinCount={cabins.length}
+       />
       <div>Todays activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -4,11 +4,13 @@ import { useSearchParams } from "react-router-dom";
 import { getBookingsAfterDate } from "../../services/apiBookings";
 
 export function useRecentBookings() {
-  const [searchParams] = useSearchParams();
+  const [ searchParams ] = useSearchParams();
 
   const numDays = !searchParams.get("last")
     ? 7
     : Number(searchParams.get("last"));
+
+
   const queryDate = subDays(new Date(), numDays).toISOString();
 
   const { isLoading, data: bookings } = useQuery({
